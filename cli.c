82f949374f3e8873648399c76bc755179e9a069c
Desai <param-ridham.desai.167@my.csun.edu>
2023-12-06 23:50:19 -0800

    fixed some bugs and did some testing

--- cli.c
+++ cli.c
@@ -7,11 +7,11 @@
 struct cell{
 
 
-	int position;
-	int adjcount;
-	int mined;
-	int flagged;
-	int covered;
+int position;
+int adjcount;
+int mined;
+int flagged;
+int covered;
 
 };
 typedef struct cell cell; // alias for struct cell as "cell"
@@ -20,69 +20,65 @@ cell **board; // 2-D array
 int rows;
 int cols;
 int mines;
-int minedarr[30];
+
 
 
 void display_cell(cell *cell){
 
-	// if refer to fields by '.' when we don't have pointers but we use '->' for refering through fields by pointers
-	if(cell->mined == 1) printf("%2s","*");
-	else if(cell->adjcount==0) printf("%2s",".");	
-	else printf("%2d",cell->adjcount);
+// if refer to fields by '.' when we don't have pointers but we use '->' for refering through fields by pointers
+if(cell->mined == 1){
+printf("%2s","*");
+}
+else if(cell->adjcount==0){
+printf("%2s",".");
+}
+else{
+printf("%2d",cell->adjcount);
+}
 }
 
 void command_show(){
 for(int i=0;i<rows;i++){
-		for(int j=0;j<cols;j++){
+for(int j=0;j<cols;j++){
 
-			display_cell(&board[i][j]);
-		}
-		printf("\n");
-	}
+display_cell(&board[i][j]);
+}
+printf("\n");
+}
 
 }
 
 
-bool mine_lookup(int row, int col){
-
-	for(int i=0;i<30;i++){
-
-		if(board[row][col].position == minedarr[i]) return true;
-
-	}
-
-	return false;
-}
 
 int get_random(int range){
 
-		return (rand()%range);
+return ((int)floor((float)range*rand()/RAND_MAX))%range;
 
 }
 
 void place_mine(){
 
 
-	if(mines ==0) return;
+if(mines ==0) return;
 
-	for(int i=1;i<=mines;i++){
+for(int i=1;i<=mines;i++){
 
-		int r  = get_random(rows);
-		int c = get_random(cols);
+int r  = get_random(rows);
+int c = get_random(cols);
 
-		while(mine_lookup(r,c) == true){
-		  
-			r=  get_random(rows);
-			c = get_random(cols);
-		}
-
-		minedarr[i] = board[r][c].position; 
-		board[r][c].mined = 1; 
-
-	//	printf("%d\n",minedarr[i]);
+while(board[r][c].mined == true){
+ 
+r=  get_random(rows);
+c = get_random(cols);
+}
 
 
-	}
+board[r][c].mined = 1;
+
+// printf("%d\n",minedarr[i]);
+
+
+}
 
 
 
@@ -98,21 +94,21 @@ int colneighbors[] = {0,1,1,1,0,-1.-1,-1};
 
 
 for(int i=0;i<rows;i++){
-	for(int j=0;j<cols;j++){
-		int minecount  = 0;
-		for(int d=0;d<neighbourcount;d++){
-		int rn = i+ rowneighbors[d];
-		//printf("%2d\n", rows);
-		int cn = j  + colneighbors[d];
-		//printf("%3d\n",cols); 
+for(int j=0;j<cols;j++){
+int minecount  = 0;
+for(int d=0;d<neighbourcount;d++){
+int rn = i+ rowneighbors[d];
+//printf("%2d\n", rows);
+int cn = j  + colneighbors[d];
+//printf("%3d\n",cols);
 
-		if(0<= rn && rn<rows && 0<=cn && cn<cols){
-			if(board[rn][cn].mined == 1) minecount++;
-		}
-		
-		}
-		board[i][j].adjcount = minecount; // there is somehting that sht obe
-	}		
+if(0<= rn && rn<rows && 0<=cn && cn<cols){
+if(board[rn][cn].mined == 1) minecount++;
+}
+
+}
+board[i][j].adjcount = minecount;
+}
 
 }
 
@@ -121,41 +117,41 @@ for(int i=0;i<rows;i++){
 
 void command_flag(int row, int col){
 
-	if(board[row][col].flagged == 0){
-		board[row][col].flagged = 1;
-	}
-	else printf("Cell %d has already been flagged\n",board[row][col].position);
+if(board[row][col].flagged == 0){
+board[row][col].flagged = 1;
+}
+else printf("Cell %d has already been flagged\n",board[row][col].position);
 }
 
 
 
 void command_unflag(int row, int col){
 
-	if(board[row][col].flagged == 1){
-		board[row][col].flagged = 0;
-		
-	}
-	else printf("Cell %d is already unflagged\n",board[row][col].position);
+if(board[row][col].flagged == 1){
+board[row][col].flagged = 0;
+
+}
+else printf("Cell %d is already unflagged\n",board[row][col].position);
 }
 
 
 void command_new(){
 
-	board =(cell **) malloc(sizeof(cell *)* rows); // memory allocation for rows
-	for(int i=0;i<rows;i++){
-	board[i] = (cell *) malloc(sizeof(cell)*cols);		// memory allocation for cols
-	}	
+board =(cell **) malloc(sizeof(cell *)* rows); // memory allocation for rows
+for(int i=0;i<rows;i++){
+board[i] = (cell *) malloc(sizeof(cell)*cols); // memory allocation for cols
+}
 
 
-	for(int i=0;i<rows;i++){
-		for(int j=0;j<cols;j++){
+for(int i=0;i<rows;i++){
+for(int j=0;j<cols;j++){
 
-			board[i][j].position = i*rows+ j;
-			display_cell(board[i]);
-		}
-	}
-	place_mine();
-	adjacencycount();
+board[i][j].position = i*rows+ j;
+display_cell(board[i]);
+}
+}
+place_mine();
+adjacencycount();
 }
 
 
@@ -167,23 +163,23 @@ int colneighbors[] = {0,1,1,1,0,-1.-1,-1};
 
 if(board[r][c].adjcount ==0){
 
-	for(int d=0;d<8;d++){
-			int rn = r + rowneighbors[d];
-			int cn = c + colneighbors[d];
-			if(0<=rn && rn<rows && 0<=cn && cn<cols){
+for(int d=0;d<8;d++){
+int rn = r + rowneighbors[d];
+int cn = c + colneighbors[d];
+if(0<=rn && rn<rows && 0<=cn && cn<cols){
 
-					if(board[rn][cn].covered ==1){
+if(board[rn][cn].covered ==1){
 
-						uncover_recursive(rn,cn);
-					}
-			}
-	}
+uncover_recursive(rn,cn);
+}
+}
+}
 
 
 }
 
 else if(board[r][c].adjcount >0 && board[r][c].mined ==0){
-		// uncover that cell
+// uncover that cell
 }
 
 else printf("Game is over, you lost!\n");
@@ -191,70 +187,78 @@ else printf("Game is over, you lost!\n");
 
 }
 int runtime(){
+srand(time(0));
 
-	 // seeding the random according to the  it runs on the computer
+// seeding the random according to the  it runs on the computer
 
-	char line[80];
-	char linecopy[80];
-	char tokens[10][80];
-	char *tok;
-	int tokencount;
-	int ranom  = get_random(10); 
-	
-
-	while(1){
-		printf(">> ");
-		fgets(line,80,stdin);
-		line[strlen(line)-1] = '\0';
-	//	printf("You entered %s\n",line);
-
-		strcpy(linecopy,line);
-		tok = strtok(linecopy," ");
-		tokencount=0;
-		while(tok != NULL){
-			strcpy(tokens[tokencount],tok);
-		//	printf("current token = %s\n",tok);
-			tok = strtok(NULL," ");
-			tokencount++;
-
-		}
-//		printf("Total tokens %d\n",tokencount);
-//		printf("name command = %s\n", tokens[0]);
-		
-		if(strcmp(tokens[0],"quit") == 0) break;
-		if(strcmp(tokens[0],"new")==0){
-		rows = atoi(tokens[1]); // atoi is used to convert strings into int, this one converts the command line argument of the first index and converts it int to string
-		cols = atoi(tokens[2]);	// atoi for columns 
-		mines= atoi(tokens[3]);	//atoi for mines
-		 command_new();
-		}
-		if(strcmp(tokens[0],"show")==0){
-		 command_show();
-		}
-		
-		if(strcmp(tokens[0],"uncover")== 0){
-		int	r = atoi(tokens[1]); // atoi is used to convert strings into int, this one converts the command line argument of the first index and converts it int to string
-		int c = atoi(tokens[2]);
-
-			uncover_recursive(r,c);
-		}
+char line[80];
+char linecopy[80];
+char tokens[10][80];
+char *tok;
+int tokencount;
+int ranom  = get_random(10);
 
 
+while(1){
+printf(">> ");
+fgets(line,80,stdin);
+line[strlen(line)-1] = '\0';
+// printf("You entered %s\n",line);
+
+strcpy(linecopy,line);
+tok = strtok(linecopy," ");
+tokencount=0;
+while(tok != NULL){
+strcpy(tokens[tokencount],tok);
+// printf("current token = %s\n",tok);
+tok = strtok(NULL," ");
+tokencount++;
+
+}
+// printf("Total tokens %d\n",tokencount);
+// printf("name command = %s\n", tokens[0]);
+
+if(strcmp(tokens[0],"quit") == 0) break;
+if(strcmp(tokens[0],"new")==0){
+rows = atoi(tokens[1]); // atoi is used to convert strings into int, this one converts the command line argument of the first index and converts it int to string
+cols = atoi(tokens[2]); // atoi for columns
+mines= atoi(tokens[3]); //atoi for mines
+command_new();
+}
+if(strcmp(tokens[0],"show")==0){
+command_show();
+}
+
+if(strcmp(tokens[0],"uncover")== 0){
+int r = atoi(tokens[1]); // atoi is used to convert strings into int, this one converts the command line argument of the first index and converts it int to string
+int c = atoi(tokens[2]);
+
+uncover_recursive(r,c);
+}
 
 
-	}
+if(strcmp(tokens[0],"flag")==0){
+int r = atoi(tokens[1]); // atoi is used to convert strings into int, this one converts the command line argument of the first index and converts it int to string
+int c = atoi(tokens[2]);
+command_flag(r,c);
+}
 
 
-	return 0;
+
+
+}
+
+
+return 0;
 }
 
 int main(void){
-	
 
-//	runtime();
-	runtime();
 
-//	display_cell();
+// runtime();
+runtime();
+
+// display_cell();
 
 }
 
